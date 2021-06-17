f74df79b53e3221e71d6952b01bc6a0f38aa3294
andres hernandez <aahrodriguez@gmail.com>
2021-06-17 10:49:43 -0300

    done

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -2,7 +2,7 @@ class Stock < ApplicationRecord
 
   def self.new_lookup(ticker_symbol)
     client = IEX::Api::Client.new(
-      publishable_token: 'Tpk_70f5aaa3087540aabf3227c2ec3988ba',
+      publishable_token:  Rails.application.credentials.iex_client[:sandbox_api_key],
       secret_token: 'Tsk_2de21b33c77946e7bc7e6a591e6eb314',
       endpoint: 'https://sandbox.iexapis.com/v1'
     )
